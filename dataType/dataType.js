dc3fea064cb89561de25bade453739fb01f0d666
hemlata-web <hemlata@rightfullabs.com>
2025-03-20 12:32:40 +0530

    Create array inertion

---
+++ dataType/dataType.js
@@ -0,0 +1,23 @@
+/*There are two types of Data Types
+1. Primitive Data Type 
+    Primitive Datatypes represent single values and are immutable.
+   1. String 
+   2. Number
+   3. Boolean
+   4. BigInt
+   5. Undefined
+   5. Null
+   7. Symbol
+2. Non- Primitive Data Type
+    Non- primitive datatypes are objects and can store collections of data or more complex entities.
+    1. Object
+    2. Array
+    3. Function 
+
+## Non- Primitive Data
+
+*/
+
+let x =3;
+let y = x++;
+console.log(`x is : ${x} and y is : ${y}`)
